4e6d9cf0e6d7ed88451e98843b0c983a56c30101
user <a@a.com>
2018-07-06 21:26:13 +0200

    rename 'myos' to 'ahos' (Ah!Os)

--- iso.sh
+++ iso.sh
@@ -6,10 +6,10 @@ mkdir -p isodir
 mkdir -p isodir/boot
 mkdir -p isodir/boot/grub
 
-cp sysroot/boot/myos.kernel isodir/boot/myos.kernel
+cp sysroot/boot/ahos.kernel isodir/boot/ahos.kernel
 cat > isodir/boot/grub/grub.cfg << EOF
-menuentry "myos" {
-	multiboot /boot/myos.kernel
+menuentry "Ah!OS" {
+	multiboot /boot/ahos.kernel
 }
 EOF
-grub-mkrescue -o myos.iso isodir
+grub-mkrescue -o ahos.iso isodir
--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -2,7 +2,19 @@
 
 #include <kernel/tty.h>
 
+static void print_banner(void)
+{
+	printf("\n\n");
+	printf("+====================+\n");
+	printf("|                    |\n");
+	printf("| Welcome to Ah!OS ! |\n");
+	printf("|                    |\n");
+	printf("======================\n");
+	printf("\n\n");
+}
+
 void kernel_main(void) {
 	terminal_initialize();
-	printf("Hello, kernel World!\n");
+
+	print_banner();
 }
