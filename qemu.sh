c6eddbc95297590bdbfbfb575b5a1a5fb16bf1ad
user <a@a.com>
2018-07-06 20:30:39 +0200

    build: qemu.sh run the kernel using '-kernel' param instead of iso image

--- qemu.sh
+++ qemu.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
-. ./iso.sh
+. ./build.sh
 
-qemu-system-$(./target-triplet-to-arch.sh $HOST) -cdrom myos.iso
+qemu-system-$(./target-triplet-to-arch.sh $HOST) -kernel sysroot/boot/myos.kernel
