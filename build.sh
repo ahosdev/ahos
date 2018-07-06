9eae73152707bc8aaa58cbbb3b114cffa5568c01
user <a@a.com>
2018-07-06 18:42:36 +0200

    first commit: minimal bootable kernel with grub(iso)/multiboot

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Build script.
+
+
+i686-elf-as boot.s -o boot.o
+i686-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+i686-elf-gcc -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
+
+if grub-file --is-x86-multiboot myos.bin; then
+	echo multiboot confirmed
+else
+	echo the file is not multiboot
+fi
