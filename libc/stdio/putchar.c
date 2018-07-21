906ca82bafd6c472d50122d19994a5bb8c6ebc0d
user <a@a.com>
2018-07-21 18:07:25 +0200

    [libc] putchar() also write to serial

--- libc/stdio/putchar.c
+++ libc/stdio/putchar.c
@@ -2,12 +2,14 @@
 
 #if defined(__is_libk)
 #include <kernel/tty.h>
+#include <kernel/serial.h>
 #endif
 
 int putchar(int ic) {
 #if defined(__is_libk)
 	char c = (char) ic;
 	terminal_write(&c, sizeof(c));
+	serial_write(&c, sizeof(c));
 #else
 	// TODO: Implement stdio and the write system call.
 #endif
