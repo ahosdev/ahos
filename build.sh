2750a272da529452bdbeb42a977017dd2abb23c8
user <a@a.com>
2018-07-29 21:32:05 +0200

    boot: added BPB description in the bootsector

--- build.sh
+++ build.sh
@@ -13,4 +13,7 @@ echo $DESTDIR
 # build the bootloader
 ${AS} ${BOOTLOADDIR}/stage1.S -o ${BOOTLOADDIR}/stage1.o
 ${LD} -Ttext 0x7c00 --oformat=binary ${BOOTLOADDIR}/stage1.o -o ${BOOTLOADDIR}/stage1.bin
+
+# TODO: replace this and build a valid MSDOS 4.0 Floppy Disk which holds
+# stage-2 and the kernel!
 dd if=${BOOTLOADDIR}/stage1.bin of=${DESTDIR}/boot.bin bs=512 count=2
