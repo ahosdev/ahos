aa2511d1e2ac265d2c67a5c8691e36e0a3f2a667
user <a@a.com>
2018-07-29 17:20:05 +0200

    boot: copy sector 2 into 0x1000:0 and jump into it

--- build.sh
+++ build.sh
@@ -13,4 +13,4 @@ echo $DESTDIR
 # build the bootloader
 ${AS} ${BOOTLOADDIR}/stage1.S -o ${BOOTLOADDIR}/stage1.o
 ${LD} -Ttext 0x7c00 --oformat=binary ${BOOTLOADDIR}/stage1.o -o ${BOOTLOADDIR}/stage1.bin
-dd if=${BOOTLOADDIR}/stage1.bin of=${DESTDIR}/boot.bin bs=512 count=1
+dd if=${BOOTLOADDIR}/stage1.bin of=${DESTDIR}/boot.bin bs=512 count=2
--- qemu-bootloader.sh
+++ qemu-bootloader.sh
@@ -2,4 +2,5 @@
 set -e
 . ./build.sh
 
-qemu-system-$(./target-triplet-to-arch.sh $HOST) -fda sysroot/boot/boot.bin -d guest_errors
+#qemu-system-$(./target-triplet-to-arch.sh $HOST) -fda sysroot/boot/boot.bin -d guest_errors -no-reboot
+qemu-system-$(./target-triplet-to-arch.sh $HOST) -fda sysroot/boot/boot.bin -d guest_errors #-no-reboot
