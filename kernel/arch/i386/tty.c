bc561abfac09bfb22e86db030d82d5dd80bf3154
user <user@user.com>
2018-12-24 01:19:59 +0100

    tty: code styling

--- kernel/arch/i386/tty.c
+++ kernel/arch/i386/tty.c
@@ -7,6 +7,10 @@
 
 #include "vga.h"
 
+// ============================================================================
+// ----------------------------------------------------------------------------
+// ============================================================================
+
 static size_t terminal_row;
 static size_t terminal_column;
 static uint8_t terminal_color;
@@ -16,13 +20,20 @@ static uint16_t* terminal_buffer;
 static uint16_t* const VGA_MEMORY = (uint16_t*) 0xB8000;
 static const size_t VGA_ELT_SIZE = sizeof(terminal_buffer[0]);
 
-void terminal_initialize(void) {
+// ============================================================================
+// ----------------------------------------------------------------------------
+// ============================================================================
+
+void terminal_initialize(void)
+{
 	terminal_row = 0;
 	terminal_column = 0;
 	terminal_default_color =
 		vga_entry_color(VGA_COLOR_BLACK, VGA_COLOR_LIGHT_GREY);
 	terminal_color = terminal_default_color;
 	terminal_buffer = VGA_MEMORY;
+
+	// clear the screen
 	for (size_t y = 0; y < VGA_HEIGHT; y++) {
 		for (size_t x = 0; x < VGA_WIDTH; x++) {
 			const size_t index = y * VGA_WIDTH + x;
@@ -31,14 +42,22 @@ void terminal_initialize(void) {
 	}
 }
 
-void terminal_setcolor(uint8_t color) {
+// ----------------------------------------------------------------------------
+
+void terminal_setcolor(uint8_t color)
+{
 	terminal_color = color;
 }
 
-void terminal_putentryat(unsigned char c, uint8_t color, size_t x, size_t y) {
+// ----------------------------------------------------------------------------
+
+void terminal_putentryat(unsigned char c, uint8_t color, size_t x, size_t y)
+{
 	const size_t index = y * VGA_WIDTH + x;
 	terminal_buffer[index] = vga_entry(c, color);
 }
+
+// ----------------------------------------------------------------------------
 
 /*
  * Moves the terminal buffer up by one line. The top line is lost.
@@ -58,7 +77,12 @@ static void scroll_up(void)
 	}
 }
 
-void terminal_putchar(char c) {
+// ============================================================================
+// ----------------------------------------------------------------------------
+// ============================================================================
+
+void terminal_putchar(char c)
+{
 	unsigned char uc = c;
 
 	if (c == '\n') {
@@ -78,11 +102,21 @@ void terminal_putchar(char c) {
 	}
 }
 
-void terminal_write(const char* data, size_t size) {
+// ----------------------------------------------------------------------------
+
+void terminal_write(const char* data, size_t size)
+{
 	for (size_t i = 0; i < size; i++)
 		terminal_putchar(data[i]);
 }
 
-void terminal_writestring(const char* data) {
+// ----------------------------------------------------------------------------
+
+void terminal_writestring(const char* data)
+{
 	terminal_write(data, strlen(data));
 }
+
+// ============================================================================
+// ----------------------------------------------------------------------------
+// ============================================================================
