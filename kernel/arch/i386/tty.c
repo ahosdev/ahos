742f4894b889c59942873d01a1b8378cdbdfa575
user <user@user.com>
2018-12-24 01:22:54 +0100

    tty: split functions in two groups: exported/static

--- kernel/arch/i386/tty.c
+++ kernel/arch/i386/tty.c
@@ -24,34 +24,14 @@ static const size_t VGA_ELT_SIZE = sizeof(terminal_buffer[0]);
 // ----------------------------------------------------------------------------
 // ============================================================================
 
-void terminal_initialize(void)
-{
-	terminal_row = 0;
-	terminal_column = 0;
-	terminal_default_color =
-		vga_entry_color(VGA_COLOR_BLACK, VGA_COLOR_LIGHT_GREY);
-	terminal_color = terminal_default_color;
-	terminal_buffer = VGA_MEMORY;
-
-	// clear the screen
-	for (size_t y = 0; y < VGA_HEIGHT; y++) {
-		for (size_t x = 0; x < VGA_WIDTH; x++) {
-			const size_t index = y * VGA_WIDTH + x;
-			terminal_buffer[index] = vga_entry(' ', terminal_color);
-		}
-	}
-}
-
-// ----------------------------------------------------------------------------
-
-void terminal_setcolor(uint8_t color)
+static void terminal_setcolor(uint8_t color)
 {
 	terminal_color = color;
 }
 
 // ----------------------------------------------------------------------------
 
-void terminal_putentryat(unsigned char c, uint8_t color, size_t x, size_t y)
+static void terminal_putentryat(unsigned char c, uint8_t color, size_t x, size_t y)
 {
 	const size_t index = y * VGA_WIDTH + x;
 	terminal_buffer[index] = vga_entry(c, color);
@@ -80,6 +60,26 @@ static void scroll_up(void)
 // ============================================================================
 // ----------------------------------------------------------------------------
 // ============================================================================
+
+void terminal_initialize(void)
+{
+	terminal_row = 0;
+	terminal_column = 0;
+	terminal_default_color =
+		vga_entry_color(VGA_COLOR_BLACK, VGA_COLOR_LIGHT_GREY);
+	terminal_setcolor(terminal_default_color);
+	terminal_buffer = VGA_MEMORY;
+
+	// clear the screen
+	for (size_t y = 0; y < VGA_HEIGHT; y++) {
+		for (size_t x = 0; x < VGA_WIDTH; x++) {
+			const size_t index = y * VGA_WIDTH + x;
+			terminal_buffer[index] = vga_entry(' ', terminal_color);
+		}
+	}
+}
+
+// ----------------------------------------------------------------------------
 
 void terminal_putchar(char c)
 {
