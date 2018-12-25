3d43fe16e6c6c3b9f3b4dbe60be455759259982a
user <user@user.com>
2018-12-25 01:42:12 +0100

    ps2ctrl: expose a 'ps2_device' interface

--- kernel/include/kernel/ps2ctrl.h
+++ kernel/include/kernel/ps2ctrl.h
@@ -13,6 +13,17 @@
 // ----------------------------------------------------------------------------
 // ============================================================================
 
+struct ps2_device {
+	char name[64];
+	bool (*init)(void *param);
+	void (*release)(void);
+	void (*recv)(uint8_t data);
+	void (*enable)(void);
+	void (*disable)(void);
+};
+
+// ----------------------------------------------------------------------------
+
 int ps2ctrl_init(void);
 bool ps2ctrl_identify_devices(void);
 void ps2ctrl_irq1_handler(void); // first port
