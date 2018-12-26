adb27847b0636c6924c46ade5bc621c9db449031
user <user@user.com>
2018-12-26 22:57:17 +0100

    iso.sh: set grub timeout to zero

--- iso.sh
+++ iso.sh
@@ -8,6 +8,7 @@ mkdir -p isodir/boot/grub
 
 cp sysroot/boot/ahos.kernel isodir/boot/ahos.kernel
 cat > isodir/boot/grub/grub.cfg << EOF
+set timeout=0
 menuentry "Ah!OS" {
 	multiboot /boot/ahos.kernel
 }
