9f749181cd864b0830c6e4b4a0a8b6abc24da614
user <user@user.com>
2018-12-26 14:26:09 +0100

    ps2ctrl: remove ps2ctrl_recv() and ps2ctrl_send() (dead code)

--- kernel/include/kernel/ps2ctrl.h
+++ kernel/include/kernel/ps2ctrl.h
@@ -19,9 +19,6 @@ void ps2ctrl_irq1_handler(void); // first port
 void ps2ctrl_irq12_handler(void); // second port
 bool ps2ctrl_cpu_reset(void);
 
-bool ps2ctrl_send(uint8_t port, uint8_t cmd);
-bool ps2ctrl_recv(uint8_t port, uint8_t *result);
-
 bool ps2ctrl_identify_devices(void);
 bool ps2ctrl_register_driver(struct ps2driver *driver);
 bool ps2ctrl_start_drivers(void);
