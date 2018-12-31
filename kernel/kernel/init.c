8b5bc5febf57782c3344a2478dc4db5e13d3d1e0
user <user@user.com>
2018-12-31 14:21:02 +0100

    init: move initialization code into 'kernel/init.c'

---
+++ kernel/kernel/init.c
@@ -0,0 +1,125 @@
+/*
+ * init.c
+ *
+ * Kernel initialization code.
+ */
+
+#include <kernel/init.h>
+#include <kernel/interrupt.h>
+#include <kernel/log.h>
+
+#include <drivers/serial.h>
+#include <drivers/clock.h>
+#include <drivers/ps2ctrl.h>
+#include <drivers/terminal.h>
+#include <drivers/keyboard.h>
+
+#include <mem/memory.h>
+
+#include <arch/gdt.h>
+
+#define LOG_MODULE "init"
+
+// ============================================================================
+// ----------------------------------------------------------------------------
+// ============================================================================
+
+static void ps2_init(void)
+{
+	info("starting PS/2 subsystem initialization...");
+
+	ps2ctrl_init();
+	keyboard_init();
+
+	info("starting PS/2 device identification...");
+	if (ps2ctrl_identify_devices() == false) {
+		error("failed to identify PS/2 devices");
+		return;
+	}
+	success("PS/2 devices identification succeed");
+
+	info("starting PS/2 drivers...");
+	if (ps2ctrl_start_drivers() == false) {
+		error("failed to start PS/2 device drivers");
+		return;
+	}
+	success("PS/2 device drivers started");
+
+	success("PS/2 subsystem initialization complete");
+}
+
+// ----------------------------------------------------------------------------
+
+static void mem_init(multiboot_info_t *mbi)
+{
+	info("initializing memory...");
+
+	if (mbi->flags & MULTIBOOT_INFO_MEM_MAP) {
+		if (phys_mem_map_init(mbi->mmap_addr, mbi->mmap_length) == false)
+		{
+			error("failed to initialize memory map");
+			abort();
+		}
+	} else {
+		error("no memory map from multiboot info, cannot initialize memory");
+		abort();
+	}
+	// we cannot use 'mbi' past this point (it is sitting in available memory)
+
+	if (pfa_init() == false) {
+		error("failed to init the page frame allocator");
+		abort();
+	}
+
+	success("memory initialization complete");
+}
+
+// ============================================================================
+// ----------------------------------------------------------------------------
+// ============================================================================
+
+/*
+ * Setup early subsystems (gdt, serial driver, terminal driver) which provides
+ * a minimal environment to access physical memory (i.e. GDT) and print
+ * debug/boot information.
+ */
+
+void kernel_early_init(void)
+{
+	// XXX: interrupts are already disabled by the bootloader
+
+	gdt_setup();
+
+	// initialise output early for debugging
+	serial_init();
+	terminal_initialize();
+}
+
+// ----------------------------------------------------------------------------
+
+void kernel_init(multiboot_info_t *mbi)
+{
+	mem_init(mbi);
+
+	setup_idt();
+	info("IDT setup");
+
+	irq_init(IRQ0_INT, IRQ7_INT); // TODO: move it into setup_idt()
+	info("IRQ initialized");
+
+	clock_init(CLOCK_FREQ);
+	info("clock initialized");
+
+	// we can re-enable interrupts now
+	info("enabling interrupts now");
+	enable_nmi();
+	enable_interrupts();
+
+	ps2_init();
+
+	success("kernel initialization complete");
+}
+
+// ============================================================================
+// ----------------------------------------------------------------------------
+// ============================================================================
--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -4,23 +4,15 @@
  * Kernel Entry Point.
  */
 
-#include <stdio.h>
-
-#include <drivers/serial.h>
-#include <drivers/clock.h>
-#include <drivers/ps2ctrl.h>
-#include <drivers/terminal.h>
-#include <drivers/keyboard.h>
-
-#include <kernel/interrupt.h>
+#include <kernel/init.h>
 #include <kernel/log.h>
 #include <kernel/scheduler.h>
 
-#include <mem/memory.h>
-
-#include <arch/gdt.h>
+#include <drivers/keyboard.h>
+#include <drivers/clock.h>
 
 #include <multiboot.h>
+#include <stdio.h>
 
 #define LOG_MODULE "main"
 
@@ -41,102 +33,6 @@ static void print_banner(void)
 	printf("\t|                    |\n");
 	printf("\t+====================+\n");
 	printf("\n\n");
-}
-
-// ----------------------------------------------------------------------------
-
-/*
- * Setup early subsystems (gdt, serial driver, terminal driver) which provides
- * a minimal environment to access physical memory and print debug/boot
- * information.
- */
-
-static void kernel_early_init(void)
-{
-	// XXX: interrupts are already disabled by the bootloader
-
-	gdt_setup();
-
-	// initialise output early for debugging
-	serial_init();
-	terminal_initialize();
-}
-
-// ----------------------------------------------------------------------------
-
-static void ps2_init(void)
-{
-	info("starting PS/2 subsystem initialization...");
-
-	ps2ctrl_init();
-	keyboard_init();
-
-	info("starting PS/2 device identification...");
-	if (ps2ctrl_identify_devices() == false) {
-		error("failed to identify PS/2 devices");
-		return;
-	}
-	success("PS/2 devices identification succeed");
-
-	info("starting PS/2 drivers...");
-	if (ps2ctrl_start_drivers() == false) {
-		error("failed to start PS/2 device drivers");
-		return;
-	}
-	success("PS/2 device drivers started");
-
-	success("PS/2 subsystem initialization complete");
-}
-
-// ----------------------------------------------------------------------------
-
-static void mem_init(multiboot_info_t *mbi)
-{
-	info("initializing memory...");
-
-	if (mbi->flags & MULTIBOOT_INFO_MEM_MAP) {
-		if (phys_mem_map_init(mbi->mmap_addr, mbi->mmap_length) == false)
-		{
-			error("failed to initialize memory map");
-			abort();
-		}
-	} else {
-		error("no memory map from multiboot info, cannot initialize memory");
-		abort();
-	}
-	// we cannot use 'mbi' past this point (it is sitting in available memory)
-
-	if (pfa_init() == false) {
-		error("failed to init the page frame allocator");
-		abort();
-	}
-
-	success("memory initialization complete");
-}
-
-// ----------------------------------------------------------------------------
-
-static void kernel_init(multiboot_info_t *mbi)
-{
-	mem_init(mbi);
-
-	setup_idt();
-	info("IDT setup");
-
-	irq_init(IRQ0_INT, IRQ7_INT); // TODO: move it into setup_idt()
-	info("IRQ initialized");
-
-	clock_init(CLOCK_FREQ);
-	info("clock initialized");
-
-	// we can re-enable interrupts now
-	info("enabling interrupts now");
-	enable_nmi();
-	enable_interrupts();
-
-	ps2_init();
-
-	success("kernel initialization complete");
 }
 
 // ----------------------------------------------------------------------------
@@ -176,6 +72,7 @@ void kernel_main(uint32_t magic, multiboot_info_t *multiboot_info)
 	}
 
 	kernel_init(multiboot_info);
+
 	print_banner();
 
 	// it only accounts from the clock initialization
