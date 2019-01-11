1d9adf22cbf8c4387201ec2d12209defd21fd5b4
user <a@a.com>
2019-01-12 00:56:39 +0100

    arch: registers: control register structures are defined in reg_t union

--- kernel/arch/i386/registers.h
+++ kernel/arch/i386/registers.h
@@ -19,53 +19,60 @@
  * Control registers definition (Intel section 2.5).
  */
 
-struct reg_cr0 {
-	uint32_t pe:1; // Protection Enable (0=real mode, 1=protected mode)
-	uint32_t mp:1; // Monitor Coprocessor
-	uint32_t em:1; // Emulation
-	uint32_t ts:1; // Task Switched
-	uint32_t et:1; // Extension Type
-	uint32_t ne:1; // Numeric Error
-	uint32_t pad0:10; // unused
-	uint32_t wp:1; // Write Protect
-	uint32_t pad1:1; // unused
-	uint32_t am:1; // Alignment Mask
-	uint32_t pad2:10; // unused
-	uint32_t nw:1; // Not Write-through
-	uint32_t cd:1; // Cache Disable
-	uint32_t pg:1; // Paging
-} __attribute__((packed));
+typedef union
+{
+	uint32_t val;
 
-struct reg_cr1 {
-	uint32_t reserved; // reserved by Intel
-} __attribute__((packed));
+	// cr0 definition
+	struct {
+		uint32_t pe:1; // Protection Enable (0=real mode, 1=protected mode)
+		uint32_t mp:1; // Monitor Coprocessor
+		uint32_t em:1; // Emulation
+		uint32_t ts:1; // Task Switched
+		uint32_t et:1; // Extension Type
+		uint32_t ne:1; // Numeric Error
+		uint32_t pad0:10; // unused
+		uint32_t wp:1; // Write Protect
+		uint32_t pad1:1; // unused
+		uint32_t am:1; // Alignment Mask
+		uint32_t pad2:10; // unused
+		uint32_t nw:1; // Not Write-through
+		uint32_t cd:1; // Cache Disable
+		uint32_t pg:1; // Paging
+	} __attribute__((packed)) cr0;
 
-struct reg_cr2 {
-	uint32_t pf_linear_address; // hold the faulty (virtual) address on #PF
-} __attribute__((packed));
+	// cr1 is reserved by intel
 
-struct reg_cr3 {
-	uint32_t pad0:3; // unused
-	uint32_t pwt:1; // Page-level Writes Transparent
-	uint32_t pcd:1; // Page-level Cache Disable
-	uint32_t pad1:7;
-	uint32_t pdb:20; // Page Directory Base address (physical)
-} __attribute__((packed));
+	// cr2 definition
+	struct {
+		uint32_t pf_addr; // hold the faulty (virtual) address on page fault
+	} __attribute__((packed)) cr2;
 
-struct reg_cr4 {
-	uint32_t vme:1; // Virtual-8086 Mode Extensions
-	uint32_t pvi:1; // Protected-Mode Virtual Interrupts
-	uint32_t tsd:1; // Time Stamp Disable
-	uint32_t de:1; // Debugging Extensions
-	uint32_t pse:1; // Page Size Extensions
-	uint32_t pae:1; // Physical Address Extension
-	uint32_t mce:1; // Machine-Check Enable
-	uint32_t pge:1; // Page Global Enable
-	uint32_t pce:1; // Performance-Monitoring Counter Enable
-	uint32_t osfxsr:1; // Operating System Support for FXSAVE/FXRSTOR instructions
-	uint32_t osxmmexcpt:1; // OS Support for Unmasked SIMD Float-Pouint32_t Exceptions
-	uint32_t reserved:21;
-} __attribute__((packed));
+	// cr3 definition
+	struct {
+		uint32_t pad0:3; // unused
+		uint32_t pwt:1; // Page-level Writes Transparent
+		uint32_t pcd:1; // Page-level Cache Disable
+		uint32_t pad1:7;
+		uint32_t pdb:20; // Page Directory Base address (physical)
+	} __attribute__((packed)) cr3;
+
+	// cr4 definition
+	struct {
+		uint32_t vme:1; // Virtual-8086 Mode Extensions
+		uint32_t pvi:1; // Protected-Mode Virtual Interrupts
+		uint32_t tsd:1; // Time Stamp Disable
+		uint32_t de:1; // Debugging Extensions
+		uint32_t pse:1; // Page Size Extensions
+		uint32_t pae:1; // Physical Address Extension
+		uint32_t mce:1; // Machine-Check Enable
+		uint32_t pge:1; // Page Global Enable
+		uint32_t pce:1; // Performance-Monitoring Counter Enable
+		uint32_t osfxsr:1; // Operating System Support for FXSAVE/FXRSTOR instructions
+		uint32_t osxmmexcpt:1; // OS Support for Unmasked SIMD Float-Pouint32_t Exceptions
+		uint32_t reserved:21;
+	} __attribute__((packed)) cr4;
+} reg_t;
 
 // ============================================================================
 // ----------------------------------------------------------------------------
