c36dad748cc87571b2a80334081734d496bbda4c
user <a@a.com>
2019-01-13 22:23:15 +0100

    build: add 'symbols.map' to ISODIR + grub loads it as multiboot module

--- iso.sh
+++ iso.sh
@@ -7,10 +7,14 @@ mkdir -p isodir/boot
 mkdir -p isodir/boot/grub
 
 cp sysroot/boot/ahos.kernel isodir/boot/ahos.kernel
+cp sysroot/boot/symbols.map isodir/boot/symbols.map
+
 cat > isodir/boot/grub/grub.cfg << EOF
 set timeout=0
 menuentry "Ah!OS" {
 	multiboot /boot/ahos.kernel
+	module /boot/symbols.map
 }
 EOF
+
 grub-mkrescue -o ahos.iso isodir
