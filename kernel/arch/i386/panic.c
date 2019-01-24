652388b9cf8ab497c9aad26cbf3cbf6ec1d8da01
user <user@user.com>
2019-01-24 12:44:19 +0100

    panic: dump call trace with symbol names

--- kernel/arch/i386/panic.c
+++ kernel/arch/i386/panic.c
@@ -4,7 +4,14 @@
  * i386 Architecture-specific panic handler.
  */
 
+#include <kernel/types.h>
+#include <kernel/symbol.h>
+
 #include <stdio.h>
+
+#include "registers.h"
+
+#define LOG_MODULE "panic"
 
 // ============================================================================
 // ----------------------------------------------------------------------------
@@ -17,14 +24,44 @@
 __attribute__((__noreturn__))
 void panic(char *msg)
 {
-	printf("=============\n");
+	/*
+	 * The stack layout is (low addresses first):
+	 * - second arg
+	 * - first arg
+	 * - return address of calling function
+	 * - EBP of calling function
+	 */
+
+	reg_t *ebp = (reg_t*)&msg - 2; // use pointer arithmetic
+
+	printf("\n=============\n");
 	printf("=== PANIC ===\n");
 	printf("=============\n\n");
 
 	printf("error: %s\n\n", msg);
 
-	// TODO
+	// dump stack trace
+	printf("Call trace:\n");
+	for (;;) {
+		struct symbol sym;
+		reg_t eip = ebp[1];
 
+		if ((eip.val == 0) || (ebp[0].val == 0)) {
+			// no more caller
+			break;
+		}
+
+		if (symbol_find((void*)eip.val, &sym)) {
+			printf("- (ebp=0x%.8x) %s() + 0x%x/0x%x\n", ebp[0].val, sym.name,
+				(eip.val - (uint32_t)sym.addr), sym.len);
+		} else {
+			printf("- (ebp=0x%.8x) ????? / 0x%x\n", ebp[0].val, eip.val);
+		}
+
+		ebp = (reg_t*) ebp[0].val;
+	}
+
+	// infinite loop
 	for (;;) { }
 	__builtin_unreachable();
 }
